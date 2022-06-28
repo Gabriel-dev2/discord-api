6c931e12f739115b434337ce082e37ee736986cb
Gabriel Lucas Costa Lima <gabriel.costa@keeggo.com>
2022-06-28 19:20:50 -0300

    Remoção de conteudo sencível

--- src/main/java/com/discord/DiscordApiApplication.java
+++ src/main/java/com/discord/DiscordApiApplication.java
@@ -8,10 +8,6 @@ import javax.security.auth.login.LoginException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-
 @SpringBootApplication
 public class DiscordApiApplication {
 
@@ -19,7 +15,6 @@ public class DiscordApiApplication {
     public void init() { TimeZone.setDefault(TimeZone.getTimeZone("America/Recife")); }
 
     public static void main(String[] args) throws LoginException {
-        //JDA jda = JDABuilder.createDefault("ODMxMjQ1MjAwNjI5NTYzNDk0.Gz6xGb.Z3Ij4HS3iBZyMIWjXilRPRsaIfkPcNidGmae14").setActivity(Activity.playing("A game")).build();
         SpringApplication.run(DiscordApiApplication.class, args);
     }
 }
